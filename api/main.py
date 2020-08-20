1f0bae512adc7f6f5432ce248193bd719a0e5f1d
Sam Kerr <samuel.k@granttree.co.uk>
2020-08-21 02:00:55 +0900

    added gift list resource and secret fix

--- api/main.py
+++ api/main.py
@@ -4,9 +4,10 @@ from flask_restful import Api
 from flask_jwt import JWT, jwt_required
 from storage import StorageInstance
 from handlers import authenticator
+from views.giftlist import GiftListView
 
 app = Flask(__name__)
-app.config['JWT_SECRET_KEY'] = environ.get('TC_SECRET_KEY')
+app.config['JWT_SECRET_KEY'] = environ.get('TC_SECRET_KEY') or ''
 app.config['JWT_AUTH_URL_RULE'] = '/api/auth'
 api = Api(app, prefix="/api")
 authenticate = authenticator.check_login_details
@@ -16,6 +17,8 @@ jwt = JWT(app, authenticate, identity)
 @app.errorhandler(403)
 def forbidden(msg):
     return make_response(jsonify({"message": msg}), 403)
+
+api.add_resource(GiftListView, '/gift_list')
 
 if __name__ == '__main__':
   from storage import User, Product
